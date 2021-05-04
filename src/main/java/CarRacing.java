732cccce1781af82078fd786cad33a8d1159470e
DDuckchul <dduckchul@naver.com>
2021-05-04 23:04:27 +0900

    fix(CarRacingString.java, CarRacing.java): 출력 메시지 수정
    CarRacingString.java
    실행시 노출되는 메시지 상수 선언
    
    CarRacing.java
    출력 샘플에 맞춰 띄어쓰기등 수정
    
    BREAKING CHANGE:
    CarRacingString.java
    상수 선언
    CarRacing.java
    hostCarRacing() - 출력 메시지 수정
    doCarRacing() - 출력 메시지 수정

--- src/main/java/CarRacing.java
+++ src/main/java/CarRacing.java
@@ -5,16 +5,22 @@ public class CarRacing {
 	private Racing racing;
 
 	public void hostCarRacing() {
+		IoUtils.printMsg(CarRacingString.INPUT_CAR_MSG);
 		String racerString = IoUtils.inputString();
 		racers = new Racers(racerString);
+
+		IoUtils.printMsg(CarRacingString.INPUT_LOOP_MSG);
 		int loops = IoUtils.inputNumbers();
 		racing = new Racing(loops);
+		IoUtils.printMsg("");
 	}
 
 	public void doCarRacing() {
+		IoUtils.printMsg(CarRacingString.START_CAR_RACING);
 		List<Car> carList = racers.getCarList();
 		for (int i = 0; i < racing.getLoop(); i++) {
 			driveLoop(carList);
+			IoUtils.printMsg("");
 		}
 	}
 
@@ -23,5 +29,9 @@ public class CarRacing {
 			racing.doRace(car);
 			IoUtils.printMsg(car.toString());
 		}
+	}
+
+	public void announceWinner() {
+
 	}
 }
--- src/main/java/CarRacingString.java
+++ src/main/java/CarRacingString.java
@@ -1,4 +1,8 @@
 public class CarRacingString {
 	public static final String RACERS_EXCEPTION_MSG = "빈 문자열은 입력 불가합니다.";
 	public static final String RACING_EXCEPTION_MSG = "최저 1바퀴를 입력해 주세요";
+
+	public static final String INPUT_CAR_MSG = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+	public static final String INPUT_LOOP_MSG = "시도할 횟수는 몇회 인가요?";
+	public static final String START_CAR_RACING = "실행 결과";
 }
