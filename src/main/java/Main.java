502b390de9537eceb9b1e8050942da32d0330f73
DDuckchul <dduckchul@naver.com>
2021-05-04 23:51:55 +0900

    feat(Main.java): Main.java 구현 main 메서드 구현
    BREAKING CHANGE: Main.java
    main()

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String [] args) {
+		CarRacing carRacing = new CarRacing();
+		carRacing.hostCarRacing();
+		carRacing.doCarRacing();
+		carRacing.announceWinner();
+	}
+}
